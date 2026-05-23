f982bf90a3493e55dbbe80c1e865b1607b364ab4
luis05rom <luis05rivera@gmail.com>
2026-05-22 20:56:27 -0600

    programa completo

--- Clase03/actividad2.py
+++ Clase03/actividad2.py
@@ -1,2 +1,33 @@
-message = "Hello, World!"
-print (message)
+print ("Binvenido al programa de calculo de IMC")
+print ("Ingrese su nombre")
+nombre = input()
+print ("Hola " + nombre)
+print ("Ingrese sus apellidos")
+apellidos = input()
+print ("Ingrese su edad")
+edad = int(input())
+print (" Ingrese su peso en kilogramos ")
+peso = float(input())
+print (" Ingrese su estatura en metros expresado en formato decimal (ejemplo: 1.75)")
+estatura = float(input())
+
+
+#Calculo del IMC
+imc = float(peso) / (float(estatura) * float(estatura))
+
+if imc < 18.5:
+    categoria = "bajo peso"
+elif imc >= 18.5 and imc < 25:
+    categoria = "peso normal"
+elif imc >= 25 and imc < 30:
+    categoria = "sobrepeso"
+else:
+    categoria = "obesidad"
+
+    #Resultado final
+    print ("ººººººººResultadosººººº")
+    
+print ("nombre: " + nombre + " " + apellidos)
+print ("edad: " + str(edad))
+print ("IMC calculado es : " + str(imc))
+print ("categoria: " + categoria)
